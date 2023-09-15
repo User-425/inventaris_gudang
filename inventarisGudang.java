75adcc9273e965b72b503055c1e868801301d29b
User-425 <user425.itsme@gmail.com>
2023-09-15 09:56:22 +0700

    added ambil

--- inventarisGudang.java
+++ inventarisGudang.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 /**
  * inventarisGudang
  */
 public class inventarisGudang {
-    int obat1 = 100, obat2 = 100; 
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int obat1 = 100, obat2 = 100;
+    int jml_ambil1, jml_ambil2;
 
     // Lihat stok
 
@@ -12,4 +17,15 @@ public class inventarisGudang {
 
     // Ambil stok
 
+    System.out.println("Ambil stok obat1 : ");
+    jml_ambil1 = input.nextInt();
+    obat1 -= jml_ambil1;
+
+    System.out.println("Ambil stok obat2  : ");
+    jml_ambil2 = input.nextInt();
+    obat2 -= jml_ambil2;
+
+    System.out.println("Stok obat1 setelah diambil: " + obat1);
+    System.out.println("Stok obat2 setelah diambil: "+ obat2);
+    }
 }
