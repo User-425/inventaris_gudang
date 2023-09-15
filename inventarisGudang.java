aec020e803fd4e692774ce075f5cdb808476d862
ArjnCrly <arjuna.carly13042gmail.com>
2023-09-15 10:06:02 +0700

    added lihat

--- inventarisGudang.java
+++ inventarisGudang.java
@@ -2,14 +2,19 @@
  * inventarisGudang
  */
 public class inventarisGudang {
+    public static void main(String[] args) {
+    
     int obat1 = 100, obat2 = 100; 
 
     // Lihat stok
-
+    System.out.println("Jumlah stok obat1: " + obat1);
+    System.out.println("Jumlah stok obat2: " + obat2); 
 
     // Tambah stok
 
 
     // Ambil stok
 
+
+    }
 }
