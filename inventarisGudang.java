8b1c983895fdf9ca9636614843aae32e7b6e632b
uhamhz <ammarhafizh2404@gmail.com>
2023-09-15 10:00:35 +0700

    added tambah

--- inventarisGudang.java
+++ inventarisGudang.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 /**
  * inventarisGudang
  */
 public class inventarisGudang {
-    int obat1 = 100, obat2 = 100; 
+
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int obat1 = 100, obat2 = 100;
+    int jml_tambah1, jml_tambah2; 
 
     // Lihat stok
 
 
     // Tambah stok
 
+    //obat1
+    System.out.println("Tambah Stok Obat 1 : ");
+    jml_tambah1 = input.nextInt();
+    obat1 += jml_tambah1;
+    
+    //obat2
+    System.out.println("Tambah Stok Obat 2 : ");
+    jml_tambah2 = input.nextInt();
+    obat2 += jml_tambah2;
+
+    //Stok
+    System.out.println("Jumlah Stok Akhir Obat 1 " + obat1);
+    System.out.println("Jumlah Stok Akhir Obat 2 " + obat2);
+    
 
     // Ambil stok
 
+    }
 }
