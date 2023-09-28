12a7a5d3ad285b56ac1c920b2827843716cc9180
uhamhz <ammarhafizh2404@gmail.com>
2023-09-29 06:38:29 +0700

    Add Switch Case

--- inventarisGudang.java
+++ inventarisGudang.java
@@ -4,33 +4,54 @@ import java.util.Scanner;
  * inventarisGudang
  */
 public class inventarisGudang {
-
+  
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int obat1 = 100, obat2 = 100;
-    int jml_tambah1, jml_tambah2; 
-
-    // Lihat stok
-
-
-    // Tambah stok
-
-    //obat1
-    System.out.println("Tambah Stok Obat 1 : ");
-    jml_tambah1 = input.nextInt();
-    obat1 += jml_tambah1;
+    int jml_tambah1, jml_tambah2,jml_ambil1, jml_ambil2;
+    char pemilihan;
     
-    //obat2
-    System.out.println("Tambah Stok Obat 2 : ");
-    jml_tambah2 = input.nextInt();
-    obat2 += jml_tambah2;
+    System.out.println("1. Lihat Stok\n2. Tambah Stok\n3. Ambil Stok");
+    pemilihan = input.next().charAt(0);
 
-    //Stok
-    System.out.println("Jumlah Stok Akhir Obat 1 " + obat1);
-    System.out.println("Jumlah Stok Akhir Obat 2 " + obat2);
+    switch (pemilihan) {
+        case '1':
+        // Lihat stok
+                System.out.println("Jumlah stok obat1: " + obat1);
+                System.out.println("Jumlah stok obat2: " + obat2); 
+            break;
+        case '2':
+        // Tambah stok
+        //obat1
+                System.out.println("Tambah Stok Obat 1 : ");
+                jml_tambah1 = input.nextInt();
+                obat1 += jml_tambah1;
     
+        //obat2
+                System.out.println("Tambah Stok Obat 2 : ");
+                jml_tambah2 = input.nextInt();
+                obat2 += jml_tambah2;
 
-    // Ambil stok
+        //Stok
+                System.out.println("Jumlah Stok Akhir Obat 1 " + obat1);
+                System.out.println("Jumlah Stok Akhir Obat 2 " + obat2);
+            break;
+        case '3':
+        // Ambil stok
+                System.out.println("Ambil stok obat1 : ");
+                jml_ambil1 = input.nextInt();
+                obat1 -= jml_ambil1;
 
+                System.out.println("Ambil stok obat2  : ");
+                jml_ambil2 = input.nextInt();
+                obat2 -= jml_ambil2;
+
+                System.out.println("Stok obat1 setelah diambil: " + obat1);
+                System.out.println("Stok obat2 setelah diambil: "+ obat2);
+            break;
+        default:
+        System.out.println("Anda Memasukan Angka yang Salah");
+            break;
+    }
     }
 }
