f1c8eede526d71eb979a272b1ecf0b1b6072313b
uhamhz <ammarhafizh2404@gmail.com>
2023-10-30 09:21:04 +0700

    Add Array 1D

--- inventarisGudang.java
+++ inventarisGudang.java
@@ -12,88 +12,89 @@ public class inventarisGudang {
         char pilihMenu;
         String user, pass, userData = "admin", userPass = "test";
         boolean login = false, running = true;
+        String[] namaObat = { "Pfizer", "Promag" };
         while (running) {
             if (login != true) {
-            // login page
-            System.out.print("============== GUDANG ==============\n");
-            System.out.print("Masukan Username : ");
-            user = input.next();
-            System.out.print("Masukan Password : ");
-            pass = input.next();
+                // login page
+                System.out.print("============== GUDANG ==============\n");
+                System.out.print("Masukan Username : ");
+                user = input.next();
+                System.out.print("Masukan Password : ");
+                pass = input.next();
 
-            if (userData.equals(user) && userPass.equals(pass)) {
-                login = true;
+                if (userData.equals(user) && userPass.equals(pass)) {
+                    login = true;
+                } else {
+                    // Notification if login invalid
+                    System.out.println("Username atau Password yang Anda Masukan Salah");
+                }
             } else {
-                // Notification if login invalid
-                System.out.println("Username atau Password yang Anda Masukan Salah");
+                // Notification if login valid
+                System.out.printf("Selamat Datang Kembali %s\n", userData);
+
+                // menu page
+                System.out.println("1.Lihat Stok\n2.Tambah Stok\n3.Ambil Stok\n4.Logout\n5.Keluar");
+                pilihMenu = input.next().charAt(0);
+
+                switch (pilihMenu) {
+                    case '1':
+                        // Lihat stok Section
+                        for (int i = 0; i < namaObat.length; i++) {
+                            System.out.println((i + 1) + ". " + namaObat[i]);
+                        }
+                        break;
+
+                    case '2':
+                        // Tambah stok Section
+                        System.out.print("============== Tambah Obat ==============\n");
+                        // obat1
+                        System.out.print("Tambah Stok Obat 1 : ");
+                        jml_tambah1 = input.nextInt();
+                        obat1 += jml_tambah1;
+                        System.out.printf("Sukses Menambahkan Obat Sebanyak %d\n", jml_tambah1);
+
+                        // obat2
+                        System.out.print("Tambah Stok Obat 2 : ");
+                        jml_tambah2 = input.nextInt();
+                        obat2 += jml_tambah2;
+                        System.out.printf("Sukses Menambahkan Obat Sebanyak %d\n", jml_tambah2);
+
+                        // Stok
+                        System.out.println("Jumlah Stok Akhir Obat 1: " + obat1);
+                        System.out.println("Jumlah Stok Akhir Obat 2: " + obat2);
+                        break;
+
+                    case '3':
+                        // Ambil stok Section
+                        System.out.print("============== Ambil Obat ==============\n");
+                        // Obat1
+                        System.out.println("Ambil stok obat1 : ");
+                        jml_ambil1 = input.nextInt();
+                        obat1 -= jml_ambil1;
+
+                        // Obat2
+                        System.out.println("Ambil stok obat2  : ");
+                        jml_ambil2 = input.nextInt();
+                        obat2 -= jml_ambil2;
+
+                        // Stok
+                        System.out.println("Stok obat1 setelah diambil: " + obat1);
+                        System.out.println("Stok obat2 setelah diambil: " + obat2);
+                        break;
+
+                    case '4':
+                        // Exit Menu
+                        login = false;
+                        break;
+                    case '5':
+                        // Exit Program Menu
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Error");
+                        break;
+                }
             }
-        } else {
-            // Notification if login valid
-            System.out.printf("Selamat Datang Kembali %s\n", userData);
-
-            // menu page
-            System.out.println("1.Lihat Stok\n2.Tambah Stok\n3.Ambil Stok\n4.Logout\n5.Keluar");
-            pilihMenu = input.next().charAt(0);
-
-            switch (pilihMenu) {
-                case '1':
-                    // Lihat stok Section
-                    System.out.print("============== Data Obat ==============\n");
-                    System.out.println("Jumlah stok obat1: " + obat1);
-                    System.out.println("Jumlah stok obat2: " + obat2);
-                    break;
-
-                case '2':
-                    // Tambah stok Section
-                    System.out.print("============== Tambah Obat ==============\n");
-                    // obat1
-                    System.out.print("Tambah Stok Obat 1 : ");
-                    jml_tambah1 = input.nextInt();
-                    obat1 += jml_tambah1;
-                    System.out.printf("Sukses Menambahkan Obat Sebanyak %d\n", jml_tambah1);
-
-                    // obat2
-                    System.out.print("Tambah Stok Obat 2 : ");
-                    jml_tambah2 = input.nextInt();
-                    obat2 += jml_tambah2;
-                    System.out.printf("Sukses Menambahkan Obat Sebanyak %d\n", jml_tambah2);
-
-                    // Stok
-                    System.out.println("Jumlah Stok Akhir Obat 1: " + obat1);
-                    System.out.println("Jumlah Stok Akhir Obat 2: " + obat2);
-                    break;
-
-                case '3':
-                    // Ambil stok Section
-                    System.out.print("============== Ambil Obat ==============\n");
-                    // Obat1
-                    System.out.println("Ambil stok obat1 : ");
-                    jml_ambil1 = input.nextInt();
-                    obat1 -= jml_ambil1;
-
-                    // Obat2
-                    System.out.println("Ambil stok obat2  : ");
-                    jml_ambil2 = input.nextInt();
-                    obat2 -= jml_ambil2;
-
-                    // Stok
-                    System.out.println("Stok obat1 setelah diambil: " + obat1);
-                    System.out.println("Stok obat2 setelah diambil: " + obat2);
-                    break;
-
-                case '4':
-                    // Exit Menu
-                    login = false;
-                    break;
-               case '5':
-                    // Exit Program Menu
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Error");
-                    break;
-            }
-        }
         }
     }
 }
